ce9ecef4eb3f4cd62c6f467c4e9162bf46d2b5b8
zenshinya <xiezhenjia88@gmail.com>
2017-04-05 22:20:14 +0800

    Added fake forecast info and to create a forecast card based on the info

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,34 @@
 
 (function() {
   'use strict';
+  
+  // Insert fake forecast info
+  var injectedForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
 
   var weatherAPIUrlBase = 'https://publicdata-weather.firebaseio.com/';
 
@@ -149,5 +177,8 @@
       app.getForecast(key);
     });
   };
+  
+  // Inject forecast and create a card
+  app.updateForecastCard(injectedForecast);
 
 })();
