b7e7f437c6b861613fd5b378e2aa07dab0d5ca08
Xie Zhenjia <xiezhenjia88@gmail.com>
2017-04-06 10:44:53 +0800

    Added script to save cities to localForage

--- scripts/app.js
+++ scripts/app.js
@@ -69,6 +69,7 @@
     var label = selected.textContent;
     app.getForecast(key, label);
     app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
 
@@ -177,8 +178,25 @@
       app.getForecast(key);
     });
   };
-  
-  // Inject forecast and create a card
-  app.updateForecastCard(injectedForecast);
 
+  app.saveSelectedCities = function() {
+    window.localforage.setItem('selectedCities', app.selectedCities);
+  };
+
+  document.addEventListener('DOMContentLoaded', function() {
+    window.localforage.getItem('selectedCities', function(err, cityList) {
+      if (cityList) {
+        app.selectedCities = cityList;
+        app.selectedCities.forEach(function(city) {
+          app.getForecast(city.key, city.label);
+        });
+      } else {
+        app.updateForecastCard(injectedForecast);
+        app.selectedCities = [
+          {key: injectedForecast.key, label: injectedForecast.label}
+        ];
+        app.saveSelectedCities();
+      }
+    });
+  });
 })();
