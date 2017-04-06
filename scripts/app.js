962bcdefdc9990fb437a035b25d616382d16282f
Xie Zhenjia <xiezhenjia88@gmail.com>
2017-04-06 11:10:57 +0800

    Added service worker

--- scripts/app.js
+++ scripts/app.js
@@ -199,4 +199,13 @@
       }
     });
   });
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+     .register('/service-worker.js')
+     .then(function() {
+        console.log('Service Worker Registered');
+      });
+  }
+
 })();
